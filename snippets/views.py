0cb7201f9e5e7d46c4d4c51b5a232b05f76feedf
re-volted <bartoszkow94@gmail.com>
2019-11-20 15:38:26 +0100

    Made use of generic APIView mixins.

--- snippets/views.py
+++ snippets/views.py
@@ -1,5 +1,5 @@
 
-from rest_framework import mixins, generics
+from rest_framework import generics
 
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
@@ -7,26 +7,11 @@ from snippets.serializers import SnippetSerializer
 # Create your views here.
 
 
-class SnippetList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
-    def get(self, req, *args, **kwargs):
-        return self.list(req, *args, **kwargs)
 
-    def post(self, req, *args, **kwargs):
-        return self.create(req, *args, **kwargs)
-
-
-class SnippetDetail(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-
-    def get(self, req, *args, **kwargs):
-        return self.retrieve(req, *args, **kwargs)
-
-    def put(self, req, *args, **kwargs):
-        return self.update(req, *args, **kwargs)
-
-    def delete(self, req, *args, **kwargs):
-        return self.destroy(req, *args, **kwargs)
